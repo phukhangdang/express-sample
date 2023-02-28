d033440b8b72991b5466fc1aa4d666a3e843b317
HO\khangdp2 <khangdp2@msb.com.vn>
2023-02-28 10:17:25 +0700

    commit

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
-
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
 app.post("/post-sample", function (req, res) {
+  console.log("/post-sample");
+
   var updateFile = function (response, body) {
     console.log(body);
 
